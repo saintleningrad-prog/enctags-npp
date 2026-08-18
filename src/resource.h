32102fc7cd6c98518b8f42189dfd97d1d9e64167
saintleningrad-prog <saintleningrad@gmail.com>
2026-08-18 09:58:35 +0300

    Add close dialog constants to resource.h

--- src/resource.h
+++ src/resource.h
@@ -6,5 +6,12 @@
 #define IDC_TAG_COUNT_LABEL 1003
 #define IDC_MODE_LOCAL      1004
 #define IDC_MODE_OPRF       1005
+
+#define IDD_CLOSE_DLG       1100
+#define IDC_CLOSE_LABEL     1101
+#define IDC_BTN_ENCRYPT     1102
+#define IDC_BTN_RETURN      1103
+#define IDC_BTN_IGNORE      1104
+
 #define IDOK                1
 #define IDCANCEL            2
